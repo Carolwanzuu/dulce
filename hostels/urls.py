2d9eba729ee97acd7572a727b05f8dcd5cb969b1
Carolwanzuu <carolkyalo01@gmail.com>
2021-07-28 15:29:30 +0300

    updating registration urls

--- hostels/urls.py
+++ hostels/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns=[
-    path('', views.hostel_list, name='hostels')
+    path('', views.hostel_list, name='hostels'),
+    path('register/', views.register, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
--- hostels/views.py
+++ hostels/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 def hostel_list(request):
     return render(request, 'index.html' )
+
+def register(request):
+    if request.method=="POST":
+        form=RegistrationForm(request.POST)
+        procForm=profileForm(request.POST, request.FILES)
+        if form.is_valid() and procForm.is_valid():
+            username=form.cleaned_data.get('username')
+            user=form.save()
+            profile=procForm.save(commit=False)
+            profile.user=user
+            profile.save()
+
+            # messages.success(request, f'Successfully created Account!.You can now login as {username}!')
+        return redirect('login')
+    else:
+        form= RegistrationForm()
+        prof=profileForm()
+    params={
+        'form':form,
+        'profForm': prof
+    }
+    return render(request, 'registration/signup.html', params)
