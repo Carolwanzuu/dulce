2a0f14f7fb1563246b1a534ab894fabadfc04efa
Carolwanzuu <carolkyalo01@gmail.com>
2021-07-28 15:42:42 +0300

    adding a superuser

--- hostels/admin.py
+++ hostels/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Hostels, Profile, Post
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Hostels)
+admin.site.register(Post)
